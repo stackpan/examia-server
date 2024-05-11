4330d796e895980d3ad0941c06aaebad44e52ae8
Ivan Rizkyanto <ivanzkyanto@outlook.com>
2024-05-11 17:15:35 +0700

    Initial commit

---
+++ src/main/java/io/github/stackpan/examia/server/examiaserver/ExamiaServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.stackpan.examia.server.examiaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamiaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamiaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/stackpan/examia/server/examiaserver/ExamiaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.stackpan.examia.server.examiaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamiaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
