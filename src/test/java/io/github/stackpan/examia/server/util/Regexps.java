abb7575405ef260af76cd8aaff9c0c708532a161
Ivan Rizkyanto <ivanzkyanto@outlook.com>
2024-06-25 15:58:01 +0700

    Fix timestamp regexp test

--- src/test/java/io/github/stackpan/examia/server/util/Regexps.java
+++ src/test/java/io/github/stackpan/examia/server/util/Regexps.java
@@ -2,7 +2,7 @@ package io.github.stackpan.examia.server.util;
 
 public class Regexps {
 
-    public static String TIMESTAMP = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{6}Z";
+    public static String TIMESTAMP = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.{0,1}\\d{1,6}Z";
 
     public static String UUID = "[0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}";
 
