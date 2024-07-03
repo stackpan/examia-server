c7f0c830471fed2eec919b0e23f71fc14da09f56
Ivan Rizkyanto <ivanzkyanto@outlook.com>
2024-07-03 20:57:03 +0700

    Fix auth tests

--- src/test/java/io/github/stackpan/examia/server/http/controller/AuthControllerTest.java
+++ src/test/java/io/github/stackpan/examia/server/http/controller/AuthControllerTest.java
@@ -1,7 +1,6 @@
 package io.github.stackpan.examia.server.http.controller;
 
 import com.jayway.jsonpath.JsonPath;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,20 +47,63 @@ public class AuthControllerTest {
                                     .contentType(MediaType.APPLICATION_JSON_VALUE)
                                     .accept(MediaType.APPLICATION_JSON_VALUE)
                                     .content(requestBody))
+                    .andExpect(status().isOk())
                     .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
                     .andExpectAll(
                             jsonPath("$.token").isString(),
                             jsonPath("$.scopes[0]").value("USER"));
+        }
+
+        @Test
+        void wrongCredentialsShouldReturnUnathorized() throws Exception {
+            var requestBody = """
+                    {
+                        "username": "wrong",
+                        "password": "wrongpassword"
+                    }
+                    """;
+
+            mockMvc.perform(
+                            post("/auth/login")
+                                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                                    .accept(MediaType.APPLICATION_JSON_VALUE)
+                                    .content(requestBody))
+                    .andExpect(status().isUnauthorized());
         }
     }
 
     @Nested
     public class Me {
 
-        private String token;
+        @Test
+        void shouldReturnCurrentUser() throws Exception {
+            var token = setupLogin();
 
-        @BeforeEach
-        void setUp() throws Exception {
+            mockMvc.perform(
+                            get("/auth/me")
+                                    .header(HttpHeaders.AUTHORIZATION, "Bearer %s".formatted(token)))
+                    .andExpect(status().isOk())
+                    .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                    .andExpectAll(
+                            jsonPath("id").value("157e4056-3a6e-4410-bc15-f14ea86887b6"),
+                            jsonPath("username").value("user"),
+                            jsonPath("email").value("user@example.com"),
+                            jsonPath("firstName").value("First"),
+                            jsonPath("lastName").value("Last"),
+                            jsonPath("role").value("USER"));
+        }
+
+        @Test
+        void invalidTokenShouldReturnUnauthorized() throws Exception {
+            var token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjRlY2U1ZmI0MjI4M2JhNjgwYWNlOTk1ZjA5ODc5NjA5IiwidHlwIjoiSldUIn0.eyJpc3MiOiJodHRwczovL2lkcC5sb2NhbCIsImF1ZCI6Im15X2NsaWVudF9hcHAiLCJzdWIiOiI1YmU4NjM1OTA3M2M0MzRiYWQyZGEzOTMyMjIyZGFiZSIsImV4cCI6MTcyMDAxNDg1NywiaWF0IjoxNzIwMDE0NTU3fQ.DRQBi4FJRjpg5iEh_qyz6sd8lwz9mupT2pii1RypkUBj0odquGRuYMjqLOAz1EC0KRCdt7tHC9ExgHj05Aa6TRHzGocOmd67iDEGf2o61APmXjn85W7egZjgzVHKZDLrpaGDDpFTAZZv4DapncKsOciLt6KZoHn9BDOqN5BNwF6SA_DYOT83QOsYwn-H4FVaBc3oiLonCMv5-uypkusmut9Z5cd3aItWNC8F7t2rl20QHY006DdPBNIv8zBxOf0knZ-CZPqNQh7OmCQdWRgFbSC-3dww2On_w0W7AqOArCe7PoRk3dz4Y7SRlj1gM59ifSgTXHxpXU1Ic5LhNZp9bA";
+
+            mockMvc.perform(
+                            get("/auth/me")
+                                    .header(HttpHeaders.AUTHORIZATION, "Bearer %s".formatted(token)))
+                    .andExpect(status().isUnauthorized());
+        }
+
+        private String setupLogin() throws Exception {
             var requestBody = """
                     {
                         "username": "user",
@@ -76,22 +118,7 @@ public class AuthControllerTest {
                                     .content(requestBody))
                     .andReturn();
 
-            token = JsonPath.read(result.getResponse().getContentAsString(), "$.token");
-        }
-
-        @Test
-        void shouldReturnCurrentUser() throws Exception {
-            mockMvc.perform(
-                            get("/auth/me")
-                                    .header(HttpHeaders.AUTHORIZATION, "Bearer %s".formatted(token)))
-                    .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                    .andExpectAll(
-                            jsonPath("id").value("157e4056-3a6e-4410-bc15-f14ea86887b6"),
-                            jsonPath("username").value("user"),
-                            jsonPath("email").value("user@example.com"),
-                            jsonPath("firstName").value("First"),
-                            jsonPath("lastName").value("Last"),
-                            jsonPath("role").value("USER"));
+            return JsonPath.read(result.getResponse().getContentAsString(), "$.token");
         }
     }
 
